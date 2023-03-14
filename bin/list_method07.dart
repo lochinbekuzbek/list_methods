f8f0a7b3b0d54c420f106c9b77c9ad568c2c0b65
lochinbekuzbek <rustamovlochin77@gmail.com>
2023-03-14 12:17:12 +0500

    ethod7

--- bin/list_method07.dart
+++ bin/list_method07.dart
@@ -6,5 +6,19 @@
     Returns:
         int: return answer
 */
+int func(List colors) {
+  int l = 0;
+  int b = 0;
+  int s = colors.length;
+  while (s > l) {
+    if (colors[l] == 0) {
+      b++;
+    }
+    l++;
+  }
+  return b;
+}
 
-void main() {}
+void main() {
+  print(func([1, 0, 0, 1, 1, 0]));
+}
